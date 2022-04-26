1743da81c9777512651cdeefafa5d642bee81813
EL HAOUITI AYOUB <elhaouitiayoub@gmail.com>
2022-04-26 12:47:26 +0200

    Comments

--- src/main/java/com/example/hospital/repositories/RendezVousRepository.java
+++ src/main/java/com/example/hospital/repositories/RendezVousRepository.java
@@ -3,5 +3,6 @@ package com.example.hospital.repositories;
 import com.example.hospital.entities.RendezVous;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//
 public interface RendezVousRepository extends JpaRepository<RendezVous, Long> {
 }
